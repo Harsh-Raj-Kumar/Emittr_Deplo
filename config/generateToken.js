2a8a005369fd971ba80ec94385853dda818daae2
Harsh-Raj-Kumar <harshrajkumar018@gmail.com>
2023-11-14 19:41:14 +0530

    changed token

--- config/generateToken.js
+++ config/generateToken.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken"); // jwt = json web token
 
 const generateToken = (id) => {
 
-  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" }); 
+  return jwt.sign({ id }, process.env.JWT_SECRET); 
   
   // generating token for 30 days
 };
